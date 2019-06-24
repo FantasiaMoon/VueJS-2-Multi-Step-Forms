c0a1c883ab2e96754c1b0f37a86303ef2670b68d
FantasiaMoon <boostio.fr@gmail.com>
2019-06-25 00:13:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:8080
+$ npm run serve
+
+# generate static project, get ready for launch on app services
+$ npm run build
+
+# lint & fixes for files
+$ npm run lint
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <MultiStepForm />
+  </div>
+</template>
+
+<script>
+  import MultiStepForm from './components/MultiStepForm.vue'
+
+  export default {
+    components: {
+      MultiStepForm
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-size: 1rem;
+    color: #444;
+    width: 100%;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+</style>
